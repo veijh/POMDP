e514222737032bcb74a233c812fa59a29525bed2
魏小爷 <844088636@qq.com>
2024-01-16 11:01:16 +0800

    7 door test OK

--- simple_maze.cpp
+++ simple_maze.cpp
@@ -127,7 +127,7 @@ int main() {
     */
 
     // unknown door
-    vector<vector<int>> unk_part{{6,7,8},{9,10,11},{13,12,14},{33,35,34},
+    vector<vector<int>> unk_part{{6,7,8},{9,10,11},{13,12,14},{33,35,34},{23,22,24},
                                      {16,17,15,18},{25,26,27,28}};
     const int doors_num = unk_part.size();
     const int all_condition_num = my_pow(2, doors_num);
